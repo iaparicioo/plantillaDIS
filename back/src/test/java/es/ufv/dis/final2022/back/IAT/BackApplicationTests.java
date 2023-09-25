32d078a1a27af2a5dfd334d64e3ca60ff1fc742c
Ivonne Aparicio <9106596@alumnos.ufv.es>
2023-09-25 17:00:32 +0200

    commit initial

--- back/src/test/java/es/ufv/dis/final2022/back/IAT/BackApplicationTests.java
+++ back/src/test/java/es/ufv/dis/final2022/back/IAT/BackApplicationTests.java
@@ -3,6 +3,7 @@ package es.ufv.dis.final2022.back.IAT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// HOLA VOY A REALIZAR UN CAMBIO
 @SpringBootTest
 class BackApplicationTests {
 
