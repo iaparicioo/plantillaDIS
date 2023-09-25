930943f673cbade668cace29e2d149f71cedc3d0
Ivonne Aparicio <9106596@alumnos.ufv.es>
2023-09-25 17:03:45 +0200

    PrimerComit

--- back/src/test/java/es/ufv/dis/final2022/back/IAT/BackApplicationTests.java
+++ back/src/test/java/es/ufv/dis/final2022/back/IAT/BackApplicationTests.java
@@ -3,7 +3,7 @@ package es.ufv.dis.final2022.back.IAT;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// HOLA VOY A REALIZAR UN CAMBIO
+// HOLA VOY A REALIZAR UN CMBIO
 @SpringBootTest
 class BackApplicationTests {
 
